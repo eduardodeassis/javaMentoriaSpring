d37bec2a358abe59fddefd2bbcf671659594c715
eduardo.assis <eduardo.assis@publicenter.com.br>
2024-10-31 12:20:49 -0300

    Alteração do nome do projeto para javaMentoriaSpring e do controlador

--- src/main/java/com/javacalcularmedia/javaMentoriaSpring/controlador/MediaControlador.java
+++ src/main/java/com/javacalcularmedia/javaMentoriaSpring/controlador/MediaControlador.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController()
-public class MediaController {
+public class MediaControlador {
 
     private MediaService mediaService;
 
-    public MediaController(MediaService mediaService) {
+    public MediaControlador(MediaService mediaService) {
         this.mediaService = mediaService;
     }
 
