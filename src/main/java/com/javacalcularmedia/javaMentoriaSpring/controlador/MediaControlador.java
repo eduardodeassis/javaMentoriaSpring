d633f2f7e58011b354545107ebf3a8bb346e8d77
eduardo.assis <eduardo.assis@publicenter.com.br>
2024-10-31 13:12:47 -0300

    Alteração para usar RequestMapping

--- src/main/java/com/javacalcularmedia/javaMentoriaSpring/controlador/MediaControlador.java
+++ src/main/java/com/javacalcularmedia/javaMentoriaSpring/controlador/MediaControlador.java
@@ -4,7 +4,11 @@ import com.javacalcularmedia.javaMentoriaSpring.entidade.Aluno;
 import com.javacalcularmedia.javaMentoriaSpring.servico.MediaServico;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 @RestController()
+@RequestMapping(value = "/ex", method = POST)
 public class MediaControlador {
 
     private MediaServico mediaServico;
@@ -13,17 +17,17 @@ public class MediaControlador {
         this.mediaServico = mediaServico;
     }
 
-    //@GetMapping("/ola")
+    @RequestMapping(value = "/ola", method = GET)
     public String ola() {
         return "Olá, Mundo!";
     }
 
-    //@GetMapping("/olaComNome/{nome}")
-    public String olaComNome(@PathVariable String nome) {
+    @RequestMapping(value = "/olaComNome{nome}", method = GET)
+    public String olaComNome(@PathVariable("nome") String nome) {
         return "Olá, " + nome + "!";
     }
 
-    //@PostMapping("/media")
+    @RequestMapping(method = POST)
     public String testeAlunoPost(@RequestBody Aluno body) {
         return mediaServico.showAluno(body);
     }
