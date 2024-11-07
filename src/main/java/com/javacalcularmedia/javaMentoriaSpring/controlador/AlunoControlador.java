9b595976ed8a5d866d130a1812654bd3172f2103
eduardo <eduardodeassis@gmail.com>
2024-11-07 20:01:30 -0300

    Implementação para listar todos os registros e calcular a média dos alunos cadastrados

--- src/main/java/com/javacalcularmedia/javaMentoriaSpring/controlador/AlunoControlador.java
+++ src/main/java/com/javacalcularmedia/javaMentoriaSpring/controlador/AlunoControlador.java
@@ -2,10 +2,9 @@ package com.javacalcularmedia.javaMentoriaSpring.controlador;
 
 import com.javacalcularmedia.javaMentoriaSpring.entidade.Aluno;
 import com.javacalcularmedia.javaMentoriaSpring.servico.AlunoServico;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/aluno")
@@ -14,6 +13,17 @@ public record AlunoControlador (AlunoServico alunoServico) {
     @PostMapping
     public Aluno salvar(@RequestBody Aluno aluno) {
         return alunoServico.salvar(aluno);
+    }
+
+    @GetMapping("/listar")
+    public List<Aluno> listarAlunos() {
+        return alunoServico.listarAlunos();
+    }
+
+    @GetMapping("/media")
+    public String mediaAlunos() {
+
+       return alunoServico.verificaMediaAlunos();
     }
 
 }
--- src/main/java/com/javacalcularmedia/javaMentoriaSpring/servico/AlunoServico.java
+++ src/main/java/com/javacalcularmedia/javaMentoriaSpring/servico/AlunoServico.java
@@ -4,9 +4,27 @@ import com.javacalcularmedia.javaMentoriaSpring.entidade.Aluno;
 import com.javacalcularmedia.javaMentoriaSpring.repositorio.AlunoRepositorio;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public record AlunoServico(AlunoRepositorio alunoRepositorio) {
     public Aluno salvar(Aluno aluno) {
         return alunoRepositorio.save(aluno);
     }
+
+    public List<Aluno> listarAlunos() {
+        return alunoRepositorio.findAll();
+    }
+
+    public String verificaMediaAlunos() {
+        MediaServico mediaServico = new MediaServico();
+        String tmp = "";
+        List<Aluno> alunos = listarAlunos();
+
+        for (Aluno aluno : alunos) {
+            tmp += String.format("%s\n\n", mediaServico.verificarAprovacao(aluno));
+        }
+        return tmp;
+    }
+
 }
